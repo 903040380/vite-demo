c2ac373465cc48969ff46828aba9128e68542f23
HHX <903040380@qq.com>
2023-02-01 18:40:08 +0800

    feat: 完成静态资源处理流程

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // 自定义的环境变量通过interface添加声明
+
+  readonly VITE_APP_TITLE: string
+  readonly VITE_IMG_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,61 @@ import react from '@vitejs/plugin-react'
 import { normalizePath } from 'vite'
 import path from 'path'
 import autoprefixer from 'autoprefixer'
+import svgr from 'vite-plugin-svgr'
+import viteImagemin from 'vite-plugin-imagemin'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // 全局 scss 文件的路径
 // 用 normalizePath 解决 window 下的路径问题
 const variablePath = normalizePath(path.resolve('./src/variable.scss'))
 
+// 是否为生产环境，在生产环境一般会注入 NODE_ENV 这个环境变量，见下面的环境变量文件配置
+const isProduction = process.env.NODE_ENV === 'production'
+// 填入项目的 CDN 域名地址
+const CDN_URL = '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // root: path.join(__dirname, 'src'),  // 手动指定项目根目录位置
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7
+      },
+      // 有损压缩配置，有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9]
+      },
+      // svg 优化
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.join(__dirname, 'src/assets/icons')]
+    })
+  ],
+
+  // 域名
+  base: isProduction ? CDN_URL : '/',
+
+  resolve: {
+    // 别名配置
+    alias: {
+      '@assets': path.join(__dirname, 'src/assets')
+    }
+  },
 
   // css 相关的配置
   css: {
@@ -43,5 +89,10 @@ export default defineConfig({
         })
       ]
     }
+  },
+
+  build: {
+    // 内嵌静态资源为base64
+    assetsInlineLimit: 3 * 1024 // 3 KB
   }
 })
